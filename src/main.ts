84118a34ed37d82799a916fd743a2ddef2942bb8
WebDevArtm <webdevartm@gmail.com>
2023-01-31 18:33:38 +0200

    generated basic structure

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000).then(() => console.log(`Server has been started on port 3000...`))
+  
+}
+bootstrap();
